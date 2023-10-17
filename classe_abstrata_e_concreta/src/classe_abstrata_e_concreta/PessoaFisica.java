ea65065587bada58c7c8e19c15fbccd1a5014716
Diego Pazzini <diegopazzini2009@hotmail.com>
2023-10-16 23:51:03 -0300

    Update PessoaFisica.java

--- classe_abstrata_e_concreta/src/classe_abstrata_e_concreta/PessoaFisica.java
+++ classe_abstrata_e_concreta/src/classe_abstrata_e_concreta/PessoaFisica.java
@@ -1,13 +1,21 @@
 package classe_abstrata_e_concreta; // Declaração do pacote
 
 public class PessoaFisica extends Pessoa { // Declaração da classe PessoaFisica que estende a classe abstrata Pessoa
+
+    private String cpf; // Declaração do atributo CPF
+
     public PessoaFisica(String nome, String endereco, String cpf) { // Construtor da classe PessoaFisica que chama o construtor da classe pai
-        super(nome, endereco, cpf); // Chamada ao construtor da classe pai com os parâmetros fornecidos
+        super(nome, endereco); // Chamada ao construtor da classe pai com os parâmetros fornecidos
+        this.cpf = cpf; // Inicialização do atributo CPF
     }
 
     @Override
     public String getIdentificacao() { // Implementação do método abstrato getIdentificacao da classe pai
         return getCpf(); // Retorna o CPF como identificação
+    }
+
+    public String getCpf() { // Definição do método para obter o CPF
+        return cpf; // Retorno do CPF
     }
 }
 
